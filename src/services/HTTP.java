f4f107a870e8488591ae11b1a1c14627314936a6
eZyloT <florian.schoeffl@gmail.com>
2015-09-03 00:03:39 +0200

    Added all HTTP Methods to detection

--- src/services/HTTP.java
+++ src/services/HTTP.java
@@ -5,17 +5,25 @@ public class HTTP extends Service {
 
     @Override
     public Boolean CheckInput(String input) {
-        return input.startsWith("GET ");
+        if(input.startsWith("GET ")) return true;
+        if(input.startsWith("POST ")) return true;
+        if(input.startsWith("HEAD ")) return true;
+        if(input.startsWith("PUT ")) return true;
+        if(input.startsWith("DELETE ")) return true;
+        if(input.startsWith("TRACE ")) return true;
+        if(input.startsWith("OPTIONS ")) return true;
+        if(input.startsWith("CONNECT ")) return true;
+        return false;
     }
 
     @Override
     public Integer getPort() {
-        return 80;
+        return 17034;
     }
 
     @Override
     public String getIP() {
-        return "127.0.0.1";
+        return "192.168.1.201";
     }
 
 }
